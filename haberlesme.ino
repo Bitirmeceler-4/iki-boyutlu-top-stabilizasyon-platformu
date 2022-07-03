4885417e0321a4bdedbda70b03475893cad522fb
Seyyid Hikmet Çelik <seyyid4091@gmail.com>
2022-07-04 02:32:50 +0300

    Add comments

--- haberlesme.ino
+++ haberlesme.ino
@@ -1,41 +1,57 @@
-// yorumlar eklenecek
+// ELE 495 Bitirme Projesi
+// Grup 4
+// Haberleşme Tarafı Bluetooth ve LCD Ekran Kodu
+// Arduino Uno
+
+// Bluetooth'tan okuma yapmayi saglayan kutuphane
 #include <NeoSWSerial.h>
+// 2x16 LCD ekran kutuphanesi
 #include <LiquidCrystal_I2C.h>
 
+// tek seferde gelen x ve y konumlarini iceren metin icin
 String xypos = "";
 
+// metnin ayristirilmasi icin
 String xstr = "";
 String ystr = "";
 
+// gelen her karakteri okumak icin
 char everyChar;
 
+// HC-05 RX-TX pinleri
 const byte rxPin = 8;
 const byte txPin = 9;
-NeoSWSerial mySerial (rxPin, txPin);
+NeoSWSerial bluSerial (rxPin, txPin);
+
 LiquidCrystal_I2C lcd(0x27,16,2);
+
 void setup() {
-Serial.begin(9600);
-mySerial.begin(9600);
+// bluetooth haberlesmesini 9600 baudrate'te baslat
+bluSerial.begin(9600);
+// LCD'yi baslat
 lcd.begin();
 }
 
 void loop() {
-    if(mySerial.available()>0)
-    lcd.clear();
-
-    while(mySerial.available() > 0) {
-        everyChar = mySerial.read();
+    // Yeni veri geldiginde LCD'yi temizle
+    if(bluSerial.available()>0)
+        lcd.clear();
+    
+    // Yeni veri gelirken donguye gir ve karakter karakter oku
+    while(bluSerial.available() > 0) {
+        everyChar = bluSerial.read();
         xypos.concat(everyChar);
     }
 
+    // Gelen metni 'x' karakterine gore farkli metinlere x ve y konumlari olarak ayristir
     if(xypos != ""){
-
       xstr = xypos.substring(0, xypos.indexOf('x'));
       ystr = xypos.substring(xypos.indexOf('x')+1, xypos.length());
     }
 
     xypos = "";
 
+    // LCD'ye x ve y konumlarini bas
     lcd.setCursor(0,0);
     lcd.print("x: " + xstr);
     lcd.setCursor(0,1);
